be12648144ea2579c56354699c77befd18a61fe8
Muhammad Alhajee <alhajee2009@gmail.com>
2025-04-29 15:42:00 +0100

    april 29 update

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -105,9 +105,10 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
+
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "SAP")
 if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+    WEBUI_NAME += " (SCIDaR AI Platform)"
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
